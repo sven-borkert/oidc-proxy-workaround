c1eb8f28afa842aa527996ebbc4e2db9e11e3ef1
Sven Borkert <sven@borkert.net>
2023-05-27 09:35:17 +0200

    Making it more generic

--- b2bi-oidc-proxy.go
+++ b2bi-oidc-proxy.go
@@ -14,7 +14,8 @@ import (
 )
 
 type Backend struct {
-	TokenEndpoint string `yaml:"token_endpoint"`
+	TokenEndpoint         string `yaml:"token_endpoint"`
+	IntrospectionEndpoint string `yaml:"introspection_endpoint"`
 }
 
 type Listener struct {
@@ -34,7 +35,25 @@ type TokenResponse struct {
 	IdToken      string `json:"id_token"`
 }
 
-func postTokenHandler(config *Config, debug *bool) http.HandlerFunc {
+type ResponseBodyModifier func(body []byte) ([]byte, error)
+
+func accessTokenToIdTokenResponseBodyModifier(body []byte) ([]byte, error) {
+	// Parse the JSON Object
+	var responseStruct TokenResponse
+	err := json.Unmarshal(body, &responseStruct)
+	if err != nil {
+		return nil, err
+	}
+	// Copy the access_token to the id_token field and re-create the JSON structure
+	responseStruct.IdToken = responseStruct.AccessToken
+	responseRaw, err := json.Marshal(responseStruct)
+	if err != nil {
+		return nil, err
+	}
+	return responseRaw, nil
+}
+
+func proxyingPostHandlerWithResponseModifier(backendUrl string, responseBodyModifier ResponseBodyModifier, config *Config, debug *bool) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			// Read the body from the request
@@ -44,7 +63,7 @@ func postTokenHandler(config *Config, debug *bool) http.HandlerFunc {
 				return
 			}
 			// Forward the request to the backend
-			backendReq, err := http.NewRequest("POST", config.Backend.TokenEndpoint, bytes.NewReader(body))
+			backendReq, err := http.NewRequest("POST", backendUrl, bytes.NewReader(body))
 			if err != nil {
 				http.Error(w, "Error creating outbound request", http.StatusInternalServerError)
 				return
@@ -58,17 +77,23 @@ func postTokenHandler(config *Config, debug *bool) http.HandlerFunc {
 				http.Error(w, "Error connecting to backend: "+err.Error(), http.StatusInternalServerError)
 				return
 			}
-			// If the backend returned a non-success response, return it to the caller
-			if backendResponse.StatusCode >= 300 || backendResponse.StatusCode < 200 {
-				http.Error(w, "Backend returned a non-success response: "+backendResponse.Status, backendResponse.StatusCode)
-				return
-			}
 			// Read the response body from the backend
 			body, err = io.ReadAll(backendResponse.Body)
 			if err != nil {
 				http.Error(w, "Error reading response from backend: "+err.Error(), http.StatusInternalServerError)
 				return
 			}
+			// If the backend returned a non-success response, return it to the caller
+			if backendResponse.StatusCode >= 300 || backendResponse.StatusCode < 200 {
+				//http.Error(w, "Backend returned a non-success response: "+backendResponse.Status, backendResponse.StatusCode)
+				_, err = w.Write(body)
+				if err != nil {
+					_, _ = fmt.Fprintln(os.Stderr, "Failure sending response to client: "+err.Error())
+					return
+				}
+				return
+			}
+
 			// Debug
 			if *debug {
 				println("Debug enabled. Dumping response from backend:")
@@ -79,28 +104,26 @@ func postTokenHandler(config *Config, debug *bool) http.HandlerFunc {
 				println("Body:")
 				println(string(body))
 			}
-			// Parse the JSON Object
-			var responseStruct TokenResponse
-			err = json.Unmarshal(body, &responseStruct)
-			if err != nil {
-				http.Error(w, "Error parsing request body: "+err.Error(), http.StatusInternalServerError)
-				return
+			// Run response body modifier, if defined
+			var responseBody []byte
+			if responseBodyModifier != nil {
+				responseBody, err = responseBodyModifier(body)
+				if err != nil {
+					http.Error(w, "Error processing request body modifier: "+err.Error(), http.StatusInternalServerError)
+					return
+				}
+			} else {
+				responseBody = body
 			}
-			// Copy the access_token to the id_token field and re-create the JSON structure
-			responseStruct.IdToken = responseStruct.AccessToken
-			responseRaw, err := json.Marshal(responseStruct)
-			if err != nil {
-				http.Error(w, "Error creating response: "+err.Error(), http.StatusInternalServerError)
-				return
-			}
+			// Return response to initial request
 			w.Header().Set("Content-Type", backendResponse.Header.Get("Content-Type"))
-			_, err = w.Write(responseRaw)
+			_, err = w.Write(responseBody)
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "Failure sending response to client: "+err.Error())
+				_, _ = fmt.Fprintln(os.Stderr, "Failure sending response to client: "+err.Error())
 				return
 			}
 		} else {
-			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			http.Error(w, "(proxyingPostHandlerWithResponseModifier) Invalid request method", http.StatusMethodNotAllowed)
 		}
 	}
 }
@@ -122,7 +145,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to unmarshal the yaml file: %s", err)
 	}
-	http.HandleFunc("/token", postTokenHandler(&config, debug))
+	http.HandleFunc("/token", proxyingPostHandlerWithResponseModifier(config.Backend.TokenEndpoint, accessTokenToIdTokenResponseBodyModifier, &config, debug))
+	http.HandleFunc("/introspection", proxyingPostHandlerWithResponseModifier(config.Backend.IntrospectionEndpoint, nil, &config, debug))
 	fmt.Printf("Starting server on port %d\n", config.Listener.ListenPort)
 	err = http.ListenAndServe(fmt.Sprintf(":%d", config.Listener.ListenPort), nil)
 	if err != nil {
